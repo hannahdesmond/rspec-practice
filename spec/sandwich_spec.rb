486bb25acd94d47d767c4cee8498fed9b8ac8a95
Hannah Desmond <desmond.hannah@googlemail.com>
2021-01-27 15:59:45 +0000

    Remove duplicate sandwich file

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,14 +1,15 @@
 Sandwich = Struct.new(:taste, :toppings)
 RSpec.describe 'An ideal sandwich' do
+  def sandwich
+    Sandwich.new('delicious', [])
+  end
   it 'is delicious' do
-    sandwich = Sandwich.new('delicious', [])
 
     taste = sandwich.taste
 
     expect(taste).to eq('delicious')
   end
   it 'lets me add toppings' do
-    sandwich = Sandwich.new('delicious', [])
 
     sandwich.toppings << 'cheese'
     toppings = sandwich.toppings
